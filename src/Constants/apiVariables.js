abe5b886c7eec5afdaeae167c5c3d32bd8462935
Mohammed Ansar Usman <mohammedansarusman@gmail.com>
2024-08-28 22:20:22 +0400

    2nd commit

--- src/Constants/apiVariables.js
+++ src/Constants/apiVariables.js
@@ -5,7 +5,7 @@ export const url2 = '&days=3';
 export const options = {
 	method: 'GET',
 	headers: {
-		'x-rapidapi-key': '1852580c53msh4d613febe8156c6p1cc646jsn028b30b01d67',
+		'x-rapidapi-key': '1852580c53msh4d613febe8156c6p1cc646jsn028b30b01d69',
 		'x-rapidapi-host': 'weatherapi-com.p.rapidapi.com'
 	}
 };
