fcf97145ed8b9255b9b98b830274b7b3a65e797a
Mohammed Ansar Usman <mohammedansarusman@gmail.com>
2024-08-28 22:34:45 +0400

    check

--- src/Constants/apiVariables.js
+++ src/Constants/apiVariables.js
@@ -5,7 +5,9 @@ export const url2 = '&days=3';
 export const options = {
 	method: 'GET',
 	headers: {
-		'x-rapidapi-key': '1852580c53msh4d613febe8156c6p1cc646jsn028b30b01d69',
+		'x-rapidapi-key': process.env.REACT_APP_WEATHER_API_KEY,
+
+		// 'x-rapidapi-key': '1852580c53msh4d613febe8156c6p1cc646jsn028b30b01d69',
 		'x-rapidapi-host': 'weatherapi-com.p.rapidapi.com'
 	}
 };
